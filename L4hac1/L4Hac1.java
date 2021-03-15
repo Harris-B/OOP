803e3afe4f5a2d918799eef1e106c28c022ae9a5
Harris-B <80236137+Harris-B@users.noreply.github.com>
2021-03-16 00:02:09 +0500

    Add files via upload

---
+++ L4hac1/L4Hac1.java
@@ -0,0 +1,15 @@
+package l4hac1;
+public class L4Hac1 {
+    public static void main(String[] args) {
+        Marks mark = new Marks();
+        mark.setMarks1(10);
+        mark.setMarks2(10);
+        mark.setMarks3(10);
+        
+        System.out.println(mark.getMarks1);
+        System.out.println(mark.getMarks2);
+        System.out.println(mark.getMarks3);
+       
+    }
+    
+}
---
+++ L4hac1/Marks.java
@@ -0,0 +1,32 @@
+package l4hac1;
+public class Marks {
+    private int marks1;
+    private int marks2;
+    private int marks3;
+
+    boolean getMarks1;
+    boolean getMarks2;
+    boolean getMarks3;
+
+    public void setMarks1(int a){
+    a=marks1;
+}
+public int getMarks1(){
+    return marks1;
+}
+public void setMarks2(int b){
+    b=marks2;
+}
+public int getMarks2(){
+    return marks2;
+}
+public void setMarks3(int c){
+    c=marks3;
+}
+public int getMarks3(){
+    return marks3;
+}
+
+        
+    
+}
